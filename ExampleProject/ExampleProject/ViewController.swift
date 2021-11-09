2b8867daf8f37f965ccf589c59433b41f8298611
Laud Bruce-Tagoe <niinyarko@yahoo.com>
2021-11-09 18:51:48 +0000

    Initial commit.

---
+++ ExampleProject/ExampleProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by Laud Bruce on 09/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var verificationButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapVerificationButton(_ sender: Any) {
+    }
+    
+}
+
