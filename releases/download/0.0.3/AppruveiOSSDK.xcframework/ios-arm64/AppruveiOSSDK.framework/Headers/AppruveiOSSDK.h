7016729aa240e6204acccc7f01efa9881674022c
Laud Bruce-Tagoe <niinyarko@yahoo.com>
2021-11-10 20:15:49 +0000

    Build for 0.0.3

---
+++ releases/download/0.0.3/AppruveiOSSDK.xcframework/ios-arm64/AppruveiOSSDK.framework/Headers/AppruveiOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  AppruveiOSSDK.h
+//  AppruveiOSSDK
+//
+//  Created by Laud Bruce on 09/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppruveiOSSDK.
+FOUNDATION_EXPORT double AppruveiOSSDKVersionNumber;
+
+//! Project version string for AppruveiOSSDK.
+FOUNDATION_EXPORT const unsigned char AppruveiOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppruveiOSSDK/PublicHeader.h>
+
+
